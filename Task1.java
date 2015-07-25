469c3ffc967608a201494c37845f3d8bebc213c3
Andrey Angelin <andreyangelin01@gmail.com>
2015-07-25 10:13:38 +0300

    25.07.15 10:00

--- Task1.java
+++ Task1.java
@@ -2,7 +2,17 @@
  * Created by Ангелин on 24.07.2015.
  */
 public class Task1 {
+    private int a, b, res;
+    // Метод который находит наибольший общий делитель двух целых положительных чисел и возвращает его.
     public int getGenericDenominator(int firstNumber, int secondNumber){
-        return 2;
+        a = firstNumber;
+        b = secondNumber;
+        for(int i = 10; i > 1; i--){
+            if (a%i == 0 && b%i== 0) {
+                res = i;
+                return;
+            }
+        }
+        return res;
     }
 }
