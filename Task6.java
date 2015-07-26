1bcd2297771f8a22e2579f2b8fd61d7f93c1e5cc
Andrey Angelin <andreyangelin01@gmail.com>
2015-07-26 14:49:02 +0300

    26.07.15 Task6

---
+++ Task6.java
@@ -0,0 +1,20 @@
+
+/**
+ * Created by Ангелин on 26.07.2015.
+ */
+public class Task6 {
+    // Метод получает в качестве параметра размер массива и возвращает массив заполненый рядом Фибоначчи.
+    public int[] getArrayOfFibonacci(int length){
+        int[] array = new int[length];
+        if (length == 1)
+            return new int[]{1};
+        else if (length == 2)
+            return new int[]{1, 1};
+        else {
+            array[0] = array[1] = 1;
+            for (int i = 2; i < length; i++)
+                array[i] = array[i-1] + array[i-2];
+        }
+        return array;
+    }
+}
