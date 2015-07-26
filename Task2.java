037ec8ee90281631ac377f6f9666f562d6fb6dbd
Andrey Angelin <andreyangelin01@gmail.com>
2015-07-26 11:34:18 +0300

    26.07.15 Task2

---
+++ Task2.java
@@ -0,0 +1,13 @@
+
+/**
+ * Created by Ангелин on 26.07.2015.
+ */
+public class Task2 {
+    private int sum = 0;
+    // Метод принимает целочисленное значение и возвращает сумму его цифр.
+    public int getSum(int val){
+        for (int i = val; i > 0; i /= 10)
+            sum += i % 10;
+        return sum;
+    }
+}
