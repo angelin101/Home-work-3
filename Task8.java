b14fd9ee43db002ee21490aee9ce3dbc150f3e45
Andrey Angelin <andreyangelin01@gmail.com>
2015-07-27 17:47:45 +0300

    27.07.15 Task8

---
+++ Task8.java
@@ -0,0 +1,18 @@
+﻿
+/**
+ * Created by Ангелин on 27.07.2015.
+ */
+public class Task8 {
+    public char[][] getChessArray(int length){
+        char[][] array = new char[length][length];
+        char color = 'Ч';
+        for (int i = 0; i < length; i ++){
+            for (int j = 0; j < length; j++){
+                array[i][j] = color;
+                color = (color =='Ч') ? 'Б' : 'Ч';
+            }
+            color = (color =='Ч') ? 'Б' : 'Ч';
+        }
+        return array;
+    }
+}
