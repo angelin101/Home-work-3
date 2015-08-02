4bc09dabb60c5b8ba8ec69da0a5ee0c099289b7e
Andrey Angelin <andreyangelin01@gmail.com>
2015-08-02 18:46:55 +0300

    Task10 change

--- Task10.java
+++ Task10.java
@@ -1,4 +1,4 @@
-package HomeWork.homeWork3;
+package hw3;
 
 /**
  * Created by Ангелин on 27.07.2015.
@@ -8,23 +8,17 @@ public class Task10 {
     // колличество строк (они же размер массива) в треугольнике Паскаля,
     // заполняет массив биноминальными коэффициентами и выводит на печать!
     public void printTriangleOfPascal(int numStr){
-        int[][] array = new int[numStr][numStr];
-        for (int n = 0; n < numStr; n++)
-            for (int m = 0; m < numStr; m++) {
-                if (n == 0) {
-                    array[0][0] = 1;
-                    continue;
-                }
+        int[][] array = new int[numStr][];
+        int a = 1;
+        for (int n = 0; n < numStr; n++) {
+            array[n] = new int[a++];
+            for (int m = 0; m < array[n].length; m++) {
                 // Вычисляеться биноминальный коэффициент по формуле - n!/m!(n-m)!
                 array[n][m] = getFact(n) / (getFact(m) * getFact((n - m)));
             }
-
+        }
         for (int n = 0; n < numStr; n++) {
-            for (int m = 0; m < numStr; m++) {
-                if (array[n][m] == 0) {
-                    System.out.print("");
-                    continue;
-                }
+            for (int m = 0; m < array[n].length; m++) {
                 System.out.print(array[n][m] + " ");
             }
             System.out.println();
